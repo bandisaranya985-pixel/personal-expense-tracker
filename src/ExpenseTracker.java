029d79f339fb54da270b3ca49a7d111efa80c5eb
bandi satya saranya <bandisaranya985@gmail.com>
2026-07-26 23:41:56 +0530

    implement add expense feature

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
 public class ExpenseTracker{
   private ArrayList<Expense>expenses = new ArrayList<>();
+  public void addExpense(Expense expense){
+    expenses.add(expense);
+  }
 }
