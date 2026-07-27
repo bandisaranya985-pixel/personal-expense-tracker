350e79b722aff1faf295fdf1d97e7272bf5d80a2
bandi satya saranya <bandisaranya985@gmail.com>
2026-07-27 22:07:47 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         tracker.addExpense(expense1);
         tracker.addExpense(expense2);
         tracker.displayExpenses();
+        System.out.println("\nFood Expenses:");
+        tracker.displayExpensesByCategory("Food");
         System.out.println("Total expenses: " + tracker.calculateTotalExpenses());
         tracker.deleteExpense(expense1);
         System.out.println("Total after deletion: " + tracker.calculateTotalExpenses());
