21e34c08f061528e462dcd123528a33d2288fcea
bandi satya saranya <bandisaranya985@gmail.com>
2026-07-27 19:30:18 +0530

    Added Expense constructor, getters, and amount validation

--- src/Expense.java
+++ src/Expense.java
@@ -2,8 +2,19 @@ public class Expense {
   private String description;
   private double amount;
   private String category;
+  public Expense(String description,double amount,String category){
+    this.description = description;
+    this.amount = amount;
+    this.category = category;
+  }
+  public String getDescription(){
+    return description;
+  }
   public double getAmount(){
     return amount;
+  }
+  public String getCategory(){
+    return category;
   }
   public void setAmount(double amount){
     if(amount > 0){
